20729c5a21856e68cf3fa312930d6225daeb3ae4
Georgi Sabev <egelon@gmail.com>
2014-01-20 17:17:40 +0200

    added code for pedometer background service

--- CardioAssistant/src/com/egelon/cardioassistant/MainActivity.java
+++ CardioAssistant/src/com/egelon/cardioassistant/MainActivity.java
@@ -1,8 +1,13 @@
 package com.egelon.cardioassistant;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.app.Activity;
+import android.content.Intent;
+import android.text.format.Time;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +23,34 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    public void openSettings ()
+    {
+    	
+    }
+    
+    public void StartPedometerService (View view) 
+    {
+    	/*
+    	 * Creates a new Intent to start the PedometerBackgroundWorker service
+    	 * IntentService.
+    	 */
+    	Intent mServiceIntent = new Intent(this, PedometerBackgroundWorker.class);
+    	//start the service
+    	startService(mServiceIntent);
     }
     
 }
